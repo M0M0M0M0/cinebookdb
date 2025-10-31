03a0541397a1055b69ec3ba3acd712703c041476
quocdang <dangvanquocmomo@gmail.com>
2025-10-31 19:53:36 +0700

    seats table

---
+++ database/migrations/2025_10_31_124735_create_seats_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id('seat_id');                    
+            $table->string('seat_row', 1);              
+            $table->integer('seat_number');          
+            $table->string('seat_type_id', 10);         
+            $table->unsignedBigInteger('room_id');     
+            $table->timestamps();                       
+        });
+
+        // Validate
+        DB::statement("ALTER TABLE seats ADD CONSTRAINT CHK_Seat_Row CHECK (LEN(seat_row) = 1)");
+        DB::statement("ALTER TABLE seats ADD CONSTRAINT CHK_Seat_Number CHECK (seat_number > 0)");
+
+        // Fk
+        Schema::table('seats', function (Blueprint $table) {
+            $table->foreign('seat_type_id')->references('seat_type_id')->on('seat_types')->onDelete('restrict');
+            $table->foreign('room_id')->references('room_id')->on('rooms')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seats');
+    }
+};

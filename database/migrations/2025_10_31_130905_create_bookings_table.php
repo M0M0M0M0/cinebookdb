39dc17ad812be74ff023257949d9df919819035f
quocdang <dangvanquocmomo@gmail.com>
2025-10-31 20:57:43 +0700

    bookings table

---
+++ database/migrations/2025_10_31_130905_create_bookings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id');
+            $table->uuid('web_user_id');
+            $table->unsignedBigInteger('showtime_id');
+            $table->dateTime('booking_date');
+            $table->string('status', 50);
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE bookings ADD CONSTRAINT CHK_Booking_Status CHECK (status IN ('open','sold-out','cancelled','completed'))");
+        DB::statement("ALTER TABLE bookings ADD CONSTRAINT CHK_Booking_Date CHECK (booking_date <= GETDATE())");
+        
+
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->foreign('showtime_id')->references('showtime_id')->on('showtimes')->onDelete('restrict');
+            $table->foreign('web_user_id')->references('web_user_id')->on('web_users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};

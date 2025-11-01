dfcba0bb76568b470216f4e5b302d0c54a2a8ba3
quocdang <dangvanquocmomo@gmail.com>
2025-11-01 12:48:34 +0700

    them api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieGenreController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/movies', function () {
+    return Movie::with('genres')->orderBy('release_date', 'desc')->limit(60)->get();
 });
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{genre}', [MovieGenreController::class, 'moviesByGenre']);
